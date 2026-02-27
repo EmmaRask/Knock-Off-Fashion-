26ff82de19dade2602f858605fe5997d79f645b6
EmmaRask <emmarask@yahoo.com>
2026-02-27 11:03:53 +0100

    productFactory.php filled with data, databaseSeeder.php  completed and seeding done

--- knockofffashion/app/Models/Product.php
+++ knockofffashion/app/Models/Product.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
-        'brand name',
+        'brand_name',
         'description',
         'price',
         'category',
