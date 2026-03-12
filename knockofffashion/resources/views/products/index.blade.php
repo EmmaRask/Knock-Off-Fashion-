2718e0c987e280ba074d94a2e6356009071665cb
EmmaRask <emmarask@yahoo.com>
2026-03-12 20:29:16 +0100

    Updated welcome phrases, store name

--- knockofffashion/resources/views/products/index.blade.php
+++ knockofffashion/resources/views/products/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
 <x-slot name="header">
-    <h2 class="text-xl font-semibold">All Products</h2>
+    <h2 class="text-xl font-semibold">All Products - Knock Off Fashion</h2>
 </x-slot>
 
 <main class="p-6">
